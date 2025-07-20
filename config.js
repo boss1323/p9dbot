c01bd9b5f74025c95178c606697f39e9b5cd37e5
boss1323 <70810620+boss1323@users.noreply.github.com>
2025-07-20 14:40:47 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   TOKEN: "",
   language: "en",
   ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "mongodb+srv://rotcharin135:<db_password>@cluster0.nimkquj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+  mongodbUri : "mongodb+srv://rotcharin135:theeraphan07@cluster0.nimkquj.mongodb.net/?retryWrites=true&w=majority",
   spotifyClientId : "",
   spotifyClientSecret : "",
   setupFilePath: './commands/setup.json',
